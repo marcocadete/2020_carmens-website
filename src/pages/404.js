266ef7c2e5a5db4323bd8341048de95cb3d465c4
Marco Cadete <marco@cadete.me>
2020-06-12 07:05:52 +0200

    Added some styling to the 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
+
+// Styles
+import styles from "../styles/notFound.module.scss"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={styles.notFound}>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <p className="cc-mb-30">
+        The page you're looking for doesn't exist, sorry.
+      </p>
+      <Link className="button is-primary" to="/">
+        Back to the home page
+      </Link>
+    </div>
   </Layout>
 )
 
