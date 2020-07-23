2a0119ebe7b8297fddf0e6cbbcee05300fbff784
Marco Cadete <marco.cadete@gmail.com>
2020-07-23 19:14:00 +0200

    Modified the SEO title for the home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Portfolio from "../components/portfolio/portfolio"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Freelance Graphic Designer" />
     <Header />
     <Services />
     <About />
